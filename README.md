ac7a8bc0234de25fb2ff70993de5cbb70a7c16d5
relaxingleg <relaxinglegcontact@gmail.com>
2022-06-30 15:41:22 +0100

    Initialized Repository

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tutorial-Bot
+This is the code for my youtube tutorial series on making a discord bot using java
+https://www.youtube.com/playlist?list=PLMDWhd7MfizXOJXn905x8UqkWtMJ6tl-b
---
+++ src/main/java/com/relaxingleg/Main.java
@@ -0,0 +1,13 @@
+package com.relaxingleg;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+
+    public static void main(String[] args) throws LoginException {
+        JDA jda = JDABuilder.createDefault(Token.TOKEN).build();
+    }
+}
